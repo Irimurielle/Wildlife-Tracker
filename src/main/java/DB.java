c7bf3607edadd02513767a22aef23e483f351019
Irimurielle <irimurielle@gmail.com>
2020-12-01 15:11:43 +0200

    added animal class and interface

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -1,4 +1,6 @@
-package PACKAGE_NAME;
+import org.sql2o.Sql2o;
 
 public class DB {
+    //    public static Sql2o sql2o = new Sql2o ("jdbc:postgresql://localhost:5432/wildlife_tracker", "murielle", "murielle12");
+    public static Sql2o sql2o = new Sql2o ("jdbc:postgresql://ec2-54-157-66-140.compute-1.amazonaws.com:5432/da5vb6eabp90pv", "uiseyvhmtdtevq", "5c26bd8e815425a3258c914a11e279cb25daaad14ee75a95b3f1209df914cc55");
 }
