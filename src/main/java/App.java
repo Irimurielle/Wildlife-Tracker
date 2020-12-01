6d612c59428a94b9a388803972ebc4377cf35f08
Irimurielle <irimurielle@gmail.com>
2020-12-01 16:13:06 +0200

    added hbs

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,21 @@
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import static spark.Spark.*;
+
 public class App {
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567;
+    }
+    public static void main(String[] args) {
+
+    }
 }
+
